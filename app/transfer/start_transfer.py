80b4594aeefe510a6f1a951dfa1f196490154c17
BrandanGG <brandan.grossi.594@gmail.com>
2024-11-17 13:45:28 +0000

    Added SFTP Support for file transfer

--- app/transfer/start_transfer.py
+++ app/transfer/start_transfer.py
@@ -4,30 +4,32 @@ from dotenv import load_dotenv  # Import load_dotenv
 
 # Load .env file located in /home/codeguard/CodeAnalysis/app/.env
 load_dotenv()
-def paramiko_con():
+def paramiko_copy(fileName: str, rand: str):
     try:
         # Fetch environment variables correctly
         IP = os.getenv('cgip')
         PORT = int(os.getenv('port'))  # Convert port to integer
         USERNAME = os.getenv('username')
         PKPATH = os.getenv('sshkey')  # Ensure this matches the correct env variable name
-
+        upload_path = f"/home/cgdocker/docker-templates/{rand}"
         print(f"Connecting to {IP} on port {PORT} as {USERNAME} with key {PKPATH}")
         
         # Initialize SSH client
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        
         # Load private key
         pk = paramiko.RSAKey(filename=PKPATH)
-
         # Connect to the server
         ssh.connect(hostname=IP, port=PORT, username=USERNAME, pkey=pk)
         print("Connected successfully to the server.")
-
         # Execute the command
-        
-        stdin, stdout, stderr = ssh.exec_command('pwd')
+        try:
+            sftp = ssh.open_sftp()
+            sftp.put(os.getenv('upload_path'), upload_path)
+            sftp.close()
+        except Exception as e:
+            print(f'Error: {e}')
+        stdin, stdout, stdeer = ssh.exec_command(f'ls {upload_path}')
         print("Command output:", stdout.read().decode())
         print("Error output:", stderr.read().decode())
         ssh.close()
